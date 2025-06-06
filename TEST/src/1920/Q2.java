f5138d516fc28697b0bdc322af8169f5f231a5af
halfapear <1783148122@qq.com>
2025-06-06 11:17:17 +0800

    6.6

--- TEST/src/1920/Q2.java
+++ TEST/src/1920/Q2.java
@@ -9,7 +9,14 @@ public class numbers(){
     */
     public int[] nums = new int[5];
 
+    //确实可以再main里面当c写 —— 确实不要想复杂了
     public static void mian(String args[]){
+        /* 
+        int[] numbers1 = new int[5];
+        numbers1[0] = 1;
+        */
+        int[] numbers2 = {1,2,3,4,5};
+        //这是什么操作 - 背吧
 
     }
 }
